3c945a61846831585040bbff42c5014bf349b674
Tsuniti <Tsuniti00@gmail.com>
2025-07-19 22:51:56 +0200

    feat: add Scalar

---
+++ Lukki.Api/Extensions/OpenApiExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Scalar.AspNetCore;
+
+namespace Lukki.Api.Extensions;
+
+public static class OpenApiExtensions
+{
+    public static void UseDefaultOpenApi(this WebApplication app)
+    {
+        app.MapOpenApi();
+
+        app.MapScalarApiReference(options =>
+        {
+            options
+                .WithTitle("Workout Service API")
+                .WithTheme(ScalarTheme.DeepSpace)
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        });
+
+        app.MapGet("/", () => Results.Redirect("/scalar/v1")).ExcludeFromDescription();
+    }
+}
--- Lukki.Api/Program.cs
+++ Lukki.Api/Program.cs
@@ -1,19 +1,24 @@
 using Lukki.Api;
+using Lukki.Api.Extensions;
 using Lukki.Application;
 using Lukki.Infrastructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddPresentation()
         .AddApplication()
-        .AddInfrastructure(builder.Configuration);
-}
+        .AddInfrastructure(builder.Configuration)
 
+        .AddOpenApi();
+}
 
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
+
+    app.UseDefaultOpenApi();
     
     app.UseHttpsRedirection();
     app.UseAuthentication();
