39c89058c73f07f10263938d260bb6920da97e4a
Tsuniti <Tsuniti00@gmail.com>
2025-09-11 17:52:43 +0200

    feat: add cors AllowFrontend

--- Lukki.Api/Program.cs
+++ Lukki.Api/Program.cs
@@ -5,18 +5,22 @@ using Lukki.Infrastructure;
 using Microsoft.AspNetCore.HttpOverrides;
 
 
-
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration)
-.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
-})
-        .AddHttpContextAccessor();
+        .AddOpenApi(options => { options.AddDocumentTransformer<BearerSecuritySchemeTransformer>(); })
+        .AddHttpContextAccessor()
+        .AddCors(
+            options =>
+            {
+                options.AddPolicy(
+                    "AllowFrontend",
+                    policy => policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+            });
+    ;
 
 
     builder.Services.Configure<ForwardedHeadersOptions>(
@@ -45,5 +49,6 @@ var app = builder.Build();
     app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
+    app.UseCors("AllowFrontend");
     app.Run();
 }
